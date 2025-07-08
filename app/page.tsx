819e51e717a0d82e0247374ade0ae68683e24854
comeonkillthecode <booraakash@gmail.com>
2025-07-08 23:51:17 +0530

    add turbo link

--- app/page.tsx
+++ app/page.tsx
@@ -307,7 +307,7 @@ export default function Home() {
           </CardContent>
         </Card>
         <a href="https://turbo0.com/item/my-goal-tracker" target="_blank" rel="noopener noreferrer">
-          <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0" style="height: 54px; width: auto;" />
+          <img src="https://img.turbo0.com/badge-listed-light.svg" alt="Listed on Turbo0" style={{height: "54px", width: "auto"}} />
         </a>
 
         <div className="text-center mt-6 text-sm text-gray-500">
